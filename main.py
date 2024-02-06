f82af08de01b19195e6638ee41285b26752e636e
sayakpaul <spsayakpaul@gmail.com>
2024-02-06 07:52:35 +0530

    support for using precomputed embeddings for the reference images.

--- main.py
+++ main.py
@@ -25,14 +25,18 @@ import numpy as np
 
 _BATCH_SIZE = flags.DEFINE_integer("batch_size", 32, "Batch size for embedding generation.")
 _MAX_COUNT = flags.DEFINE_integer("max_count", -1, "Maximum number of images to read from each directory.")
+_REF_EMBED_FILE = flags.DEFINE_string(
+    "ref_embed_file", None, "Path to the pre-computed embedding file for the reference images."
+)
 
 
-def compute_cmmd(ref_dir, eval_dir, batch_size=32, max_count=-1):
+def compute_cmmd(ref_dir, eval_dir, ref_embed_file=None, batch_size=32, max_count=-1):
     """Calculates the CMMD distance between reference and eval image sets.
 
     Args:
       ref_dir: Path to the directory containing reference images.
       eval_dir: Path to the directory containing images to be evaluated.
+      ref_embed_file: Path to the pre-computed embedding file for the reference images.
       batch_size: Batch size used in the CLIP embedding calculation.
       max_count: Maximum number of images to use from each directory. A
         non-positive value reads all images available except for the images
@@ -41,8 +45,15 @@ def compute_cmmd(ref_dir, eval_dir, batch_size=32, max_count=-1):
     Returns:
       The CMMD value between the image sets.
     """
+    if ref_dir and ref_embed_file:
+        raise ValueError("`ref_dir` and `ref_embed_file` both cannot be set at the same time.")
     embedding_model = embedding.ClipEmbeddingModel()
-    ref_embs = io_util.compute_embeddings_for_dir(ref_dir, embedding_model, batch_size, max_count).astype("float32")
+    if ref_embed_file is not None:
+        ref_embs = np.load(ref_embed_file).astype("float32")
+    else:
+        ref_embs = io_util.compute_embeddings_for_dir(ref_dir, embedding_model, batch_size, max_count).astype(
+            "float32"
+        )
     eval_embs = io_util.compute_embeddings_for_dir(eval_dir, embedding_model, batch_size, max_count).astype("float32")
     val = distance.mmd(ref_embs, eval_embs)
     return val.numpy()
@@ -52,7 +63,10 @@ def main(argv):
     if len(argv) != 3:
         raise app.UsageError("Too few/too many command-line arguments.")
     _, dir1, dir2 = argv
-    print("The CMMD value is: " f" {compute_cmmd(dir1, dir2, _BATCH_SIZE.value, _MAX_COUNT.value):.3f}")
+    print(
+        "The CMMD value is: "
+        f" {compute_cmmd(dir1, dir2, _REF_EMBED_FILE.value, _BATCH_SIZE.value, _MAX_COUNT.value):.3f}"
+    )
 
 
 if __name__ == "__main__":
